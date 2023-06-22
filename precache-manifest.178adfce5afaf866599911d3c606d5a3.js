7eba13eb664ee65fb35253779de6db090d7ff7b8
sumit.verma <sumit.verma@naukri.com>
2023-06-22 13:24:24 +0530

    Updates

---
+++ precache-manifest.178adfce5afaf866599911d3c606d5a3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e776a9c7e4e2ccb850df90048a21f81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "348965bb90b1b330df95",
+    "url": "/static/css/createPost.ddb10bbd.chunk.css"
+  },
+  {
+    "revision": "3c60a997ced51857b46d",
+    "url": "/static/css/login.75118113.chunk.css"
+  },
+  {
+    "revision": "c203aba3c378f56aee42",
+    "url": "/static/css/main.2592a8e8.chunk.css"
+  },
+  {
+    "revision": "2f09afc7d379b2013fa4",
+    "url": "/static/css/post.33777b03.chunk.css"
+  },
+  {
+    "revision": "a3415f271bd88d98dbed",
+    "url": "/static/css/singup.d28be62b.chunk.css"
+  },
+  {
+    "revision": "78432de2eeb5df30c973",
+    "url": "/static/js/6.d3e34dc1.chunk.js"
+  },
+  {
+    "revision": "b50a64d96f73ba49c03b",
+    "url": "/static/js/7.72254f41.chunk.js"
+  },
+  {
+    "revision": "ff9ef0d495307d9cc322",
+    "url": "/static/js/8.707cd673.chunk.js"
+  },
+  {
+    "revision": "348965bb90b1b330df95",
+    "url": "/static/js/createPost.80ae2ab4.chunk.js"
+  },
+  {
+    "revision": "3c60a997ced51857b46d",
+    "url": "/static/js/login.6d57bbf6.chunk.js"
+  },
+  {
+    "revision": "c203aba3c378f56aee42",
+    "url": "/static/js/main.44da57e0.chunk.js"
+  },
+  {
+    "revision": "2f09afc7d379b2013fa4",
+    "url": "/static/js/post.94dac209.chunk.js"
+  },
+  {
+    "revision": "12adee3553183cf51e8a",
+    "url": "/static/js/runtime~main.44006a30.js"
+  },
+  {
+    "revision": "a3415f271bd88d98dbed",
+    "url": "/static/js/singup.f6e2418e.chunk.js"
+  },
+  {
+    "revision": "625b448e59ca41473df60cfba55f5c79",
+    "url": "/static/media/dummy_post_img.625b448e.png"
+  },
+  {
+    "revision": "86851ac52ba1b613e9f3b18c12f1ce98",
+    "url": "/static/media/dummy_profile_img.86851ac5.png"
+  },
+  {
+    "revision": "ec498b877495146831f16b704ff8dc7c",
+    "url": "/static/media/plus-icon.ec498b87.png"
+  }
+]);
